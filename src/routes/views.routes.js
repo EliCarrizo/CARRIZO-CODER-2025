fab55a55be0924cee60e960abe7229fb5b238c2f
Eliana Carrizo <elianag.carrizo@gmail.com>
2025-08-02 11:46:37 -0300

    Change and put views, modified routes

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -13,3 +13,14 @@ router.get('/realtimeproducts', async (req, res) => {
 });
 
 module.exports = router;
+
+router.get("/products/:pid", async (req, res) => {
+  const product = await productModel.findById(req.params.pid).lean();
+  res.render("productDetail", { product });
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  const cart = await cartModel.findById(req.params.cid).populate("products.product").lean();
+  res.render("cart", { cart });
+});
+
